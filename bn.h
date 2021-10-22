10c30af807adfc54b57d6d11e26bf7a2852d0722
bumbread <thebumboni@gmail.com>
2021-10-22 18:25:19 +1100

    update inline docs, add overflow handling routines

--- bn.h
+++ bn.h
@@ -11,7 +11,6 @@ Licence: public domain.
 *****************************************************************************/
 
 #include <stdint.h>
-#include <stdbool.h>
 #include <inttypes.h>
 
 #if !defined(bn_assert)
@@ -23,42 +22,170 @@ Licence: public domain.
   #define bn_array_size 32
 #endif
 
-#if bn_array_size <= 0
-  #error "bn_array_size must be at least 1"
+#if bn_array_size <= 1
+  #error "bn_array_size must be at least 2"
 #endif
 
 #define bn_word_msb (UINT64_C(0x80000000))
 #define bn_max_val  (UINT64_C(0xFFFFFFFF))
 
-struct bn
+typedef struct Bignum Bignum;
+struct Bignum
 {
   uint32_t array[bn_array_size];
 };
 
-void bignum_init(struct bn* n);
-void bignum_from_int(struct bn* n, uint64_t i);
-int  bignum_to_int(struct bn* n);
-void bignum_from_string(struct bn* n, char* str, int nbytes);
-void bignum_to_string(struct bn* n, char* str, int maxsize);
+/*****************************************************************************
+  Initialization functions.
+  The  first  argument to all these functions is a pointer to the bignum to be
+  initialized.
 
-void bignum_incr(struct bn* n);
-void bignum_decr(struct bn* n);
-void bignum_add(struct bn* a, struct bn* b, struct bn* c);
-void bignum_sub(struct bn* a, struct bn* b, struct bn* c);
-void bignum_mul(struct bn* a, struct bn* b, struct bn* c);
-void bignum_div(struct bn* a, struct bn* b, struct bn* c);
-void bignum_mod(struct bn* a, struct bn* b, struct bn* c);
-void bignum_divmod(struct bn* q, struct bn *r, struct bn* lhs, struct bn* rhs);
+  1. `bignum_init`
+    Takes  in  a  reference  to  Bignum  variable  and initializes it to zero.
+    
+    Alternatively you can initialize Bignum variables with
+    `variable = {0};` in C or `variable = {};` in C++.
 
-int  bignum_cmp(struct bn* a, struct bn* b);
-int  bignum_is_zero(struct bn* n);
-void bignum_assign(struct bn* dst, struct bn* src);
+  2. `bignum_from_u64`
+    Takes  64-bit unsigned integer and initializes Bignum with the same value.
+
+  3. `bignum_from_hex`
+    Takes  a  string  with  HEX  representation  of  a bignum, and initializes
+    a  Bignum variable with the value encoded in the string. The string has to
+    be multiple of 8 characters.
+
+    `maxsize`  has  to  represent the actual size of the string (or less), and
+    be a multiple of 8.
+
+  4. `bignum_assign`
+    Takes  initalized  `src`  Bignum, and copies it's value to `dst` variable,
+    initializing it.
+
+*****************************************************************************/
+void bignum_init(Bignum* n);
+void bignum_from_u64(Bignum* n, uint64_t i);
+void bignum_from_hex(Bignum* n, char const* str, int maxsize);
+void bignum_assign(Bignum* dst, Bignum const* src);
+
+
+/*****************************************************************************
+  Getting the value out of bignum.
+
+  1. `u64_from_bignum`
+    Takes an initialized Bignum integer and converts it to u64. If the value
+    of the Bignum is not representable by u64, it is truncated to the lowest
+    64  bits  and  the  Overflow Flag is set. For more information about the
+    overflow flag see the section below.
+
+  2. `hex_from_bignum`
+    Takes  a  finite  string  of  hex  digits  and  converts  it  to a bignum.
+    `maxsize`  must  be no greater than the size of the `str`. The digits will
+    be   written  in  the  big-endian  format,  with  the  lowest-order  digit
+    necessarily being written in the (maxsize-1)-th's character of the string.
+*****************************************************************************/
+uint64_t u64_from_bignum(Bignum const* n);
+void hex_from_bignum(Bignum const* n, char* str, int maxsize);
+
+
+/*****************************************************************************
+  Overflow management
+
+  1. `bignum_reset_overflow_flag`
+    Resets  overflow  flag,  meaning that it will be treated as if no overflow
+      had occured.
+
+  2. `bignum_is_overflow`
+    Returns the value of the overflow flag. Zero, if the overflow wasn't
+      occured, and non-zero value if it has.
+*****************************************************************************/
+void bignum_reset_overflow_flag(void);
+int bignum_is_overflow(void);
+
+/*****************************************************************************
+  Comparison functions
+
+  1. `bignum_cmp`
+    Returns negative value, if `a` is smaller than `b`
+            0,              if `a` and `b` are equal
+            positive value, if `a` is greater than `b`
+
+  2. `bignum_greater`
+    Returns non-zero value if `a` is greater than `b`
+    Returns zero, if `a` is less than or equal to `b`
+
+  3. `bignum_less`
+    Returns non-zero value if `a` is less than `b`
+    Returns zero, if `a` is greater than or equal to `b`
+
+  4. `bignum_geq`
+    Returns non-zero value if `a` is greater than or equal to `b`
+    Returns zero if `a` is less than `b`
+
+  5. `bignum_leq`
+    Returns non-zero value if `a` is less than or equal to `b`
+    Returns zero if `a` is greater than `b`
+
+  6. `bignum_is_zero`
+    Returns non-zero value if `n` is zero
+    Returns zero if `n` is non-zero.
+*****************************************************************************/
+int  bignum_cmp(Bignum const* a, Bignum const* b);
+int  bignum_greater(Bignum const* a, Bignum const* b);
+int  bignum_less(Bignum const* a, Bignum const* b);
+int  bignum_geq(Bignum const* a, Bignum const* b);
+int  bignum_leq(Bignum const* a, Bignum const* b);
+int  bignum_equal(Bignum const* a, Bignum const* b);
+int  bignum_is_zero(Bignum const* n);
+
+
+/*****************************************************************************
+  Arithmetic functions
+  
+  The result pointer of the functions is allowed to point to the same object
+  as the left-hand side operand of the corresponding operation.
+
+  If the result of the operation overflows (becomes unrepresentable by current
+  size), for example being higher than 2**(4*bn_array_size), or less than zero
+  then  the overflow flag is set, and you can check it with bignum_is_overflow
+  function.
+
+  1. `bignum_incr`
+    Increases  the  value of `n` by 1. If `n` overflows, overflow flag is set.
+
+  2. `bignum_decr`
+    Decreases   the  value of `n` by 1.
+  
+  3. `bignum_add`
+    Adds  the  values  of  `b`  and `c` and stores the resulting value in `a`.
+
+  4. `bignum_sub`
+    Sutracts  the values of `b` and `c` and stores the resulting value in `a`.
+
+  5. `bignum_mul`
+    Multiplies  the numbers `b` and `c` and stores the resulting value in `a`.
+
+  6. `bignum_div`
+    Divides the numbers `a` by `b`, stores the quotient of the division in
+    `q`, and the remainder in `r`.
+*****************************************************************************/
+void bignum_incr(Bignum* n);
+void bignum_decr(Bignum* n);
+void bignum_add(Bignum* a, Bignum const* b, Bignum const* c);
+void bignum_sub(Bignum* a, Bignum const* b, Bignum const* c);
+void bignum_mul(Bignum* a, Bignum const* b, Bignum const* c);
+void bignum_divmod(Bignum* q, Bignum *r, Bignum const* a, Bignum const* b);
+
+/*****************************************************************************
+  END OF HEADING
+*****************************************************************************/
+
+
 
 #ifdef bn_implementation
 
-static bool bn_overflow_flag = 0;
+static int bn_overflow_flag = 0;
 
-void bignum_init(struct bn* n)
+void bignum_init(Bignum* n)
 {
   bn_assert(n);
 
@@ -69,8 +196,7 @@ void bignum_init(struct bn* n)
   }
 }
 
-
-void bn_from_u64(struct bn* bn, uint64_t n)
+void bignum_from_u64(Bignum* bn, uint64_t n)
 {
   bn_assert(bn);
   bignum_init(bn);
@@ -79,33 +205,25 @@ void bn_from_u64(struct bn* bn, uint64_t n)
   bn->array[1] = n >> 32;
 }
 
-
-int bignum_to_int(struct bn* n)
-{
-  bn_assert(n);
-  return n->array[0];
-}
-
 static inline int hexchar__to_int(char a)
 {
   if('a' <= a && a <= 'f') return a-'a'+10;
   else return a-'0';
 }
 
-void bignum_from_string(struct bn* n, char* str, int nbytes)
+void bignum_from_hex(Bignum* n, char const* str, int maxsize)
 {
   bn_assert(n);
   bn_assert(str);
-  bn_assert(nbytes > 0);
-  bn_assert((nbytes & 1) == 0);
-  bn_assert((nbytes % (sizeof(uint32_t) * 2)) == 0);
+  bn_assert(maxsize > 0);
+  bn_assert(maxsize%2 == 0);
   
   bignum_init(n);
 
   const int chars_per_u32 = 2*sizeof(uint32_t);
 
   uint32_t tmp;
-  int i = nbytes - chars_per_u32;
+  int i = maxsize - chars_per_u32;
   int j = 0;
   while (i >= 0)
   {
@@ -119,6 +237,26 @@ void bignum_from_string(struct bn* n, char* str, int nbytes)
     i -= chars_per_u32;
     j += 1;
   }
+}
+
+void bignum_assign(Bignum* dst, Bignum const* src)
+{
+  bn_assert(dst);
+  bn_assert(src);
+
+  int i;
+  for (i = 0; i < bn_array_size; ++i)
+  {
+    dst->array[i] = src->array[i];
+  }
+}
+
+
+
+uint64_t u64_from_bignum(Bignum const* n)
+{
+  bn_assert(n);
+  return n->array[0] | (n->array[1] << 1);
 }
 
 static inline char bn__hexlo(uint8_t b)
@@ -137,17 +275,17 @@ static inline char bn__hexhi(uint8_t b)
   return q;
 }
 
-void bignum_to_string(struct bn* n, char* str, int nbytes)
+void hex_from_bignum(Bignum const* n, char* str, int maxsize)
 {
   bn_assert(n);
   bn_assert(str);
-  bn_assert(nbytes > 0);
-  bn_assert((nbytes & 1) == 0);
+  bn_assert(maxsize > 0);
+  bn_assert(maxsize%8 == 0);
 
   int j = bn_array_size - 1;
   int i = 0;
 
-  while ((j >= 0) && (nbytes > (i + 1)))
+  while ((j >= 0) && (maxsize > (i + 1)))
   {
     str[i+0] = bn__hexhi(n->array[j]>>16);
     str[i+1] = bn__hexlo(n->array[j]>>16);
@@ -165,7 +303,7 @@ void bignum_to_string(struct bn* n, char* str, int nbytes)
     j += 1;
   }
  
-  for (i = 0; i < (nbytes - j); ++i)
+  for (i = 0; i < maxsize-j; ++i)
   {
     str[i] = str[i + j];
   }
@@ -174,49 +312,50 @@ void bignum_to_string(struct bn* n, char* str, int nbytes)
 }
 
 
-void bignum_decr(struct bn* n)
+
+void bignum_reset_overflow_flag(void)
 {
-  bn_assert(n);
+  bn_overflow_flag = 0;
+}
 
-  bool carry = 1;
-  for(int i = 0; i != bn_array_size; ++i)
-  {
-    n->array[i] -= 1;
-
-    // Note(bumbread): If decrement overflowed, the new
-    // value would be 0xFFFFFFFF
-    if(n->array[i] != 0xFFFFFFFF) {
-      carry = 0;
-      break;
-    }
-  }
-  
-  bn_overflow_flag = (carry==1);
+int bignum_is_overflow(void)
+{
+  return bn_overflow_flag;
 }
 
 
-void bignum_incr(struct bn* n)
+
+void bignum_incr(Bignum* n)
 {
   bn_assert(n);
-
-  bool carry = 1;
   for(int i = 0; i != bn_array_size; ++i)
   {
     n->array[i] += 1;
-
     // Note(bumbread): If increment overflowed, the new
     // value would be 0x00000000
-    if(n->array[i] != 0) {
-      carry = 0;
+    if(n->array[i] == 0) {
+      bn_overflow_flag |= 1;
       break;
     }
   }
-  
-  bn_overflow_flag = (carry==1);
 }
 
+void bignum_decr(Bignum* n)
+{
+  bn_assert(n);
+  for(int i = 0; i != bn_array_size; ++i)
+  {
+    n->array[i] -= 1;
+    // Note(bumbread): If decrement overflowed, the new
+    // value would be 0xFFFFFFFF
+    if(n->array[i] != 0xFFFFFFFF) {
+      bn_overflow_flag |= 1;
+      break;
+    }
+  }  
+}
 
-void bignum_add(struct bn* res, struct bn* lhs, struct bn* rhs)
+void bignum_add(Bignum* res, Bignum const* lhs, Bignum const* rhs)
 {
   bn_assert(res);
   bn_assert(lhs);
@@ -236,7 +375,7 @@ void bignum_add(struct bn* res, struct bn* lhs, struct bn* rhs)
 }
 
 
-void bignum_sub(struct bn* res, struct bn* lhs, struct bn* rhs)
+void bignum_sub(Bignum* res, Bignum const* lhs, Bignum const* rhs)
 {
   bn_assert(res);
   bn_assert(lhs);
@@ -253,20 +392,20 @@ void bignum_sub(struct bn* res, struct bn* lhs, struct bn* rhs)
 }
 
 
-void bignum_mul(struct bn* res, struct bn* lhs, struct bn* rhs)
+void bignum_mul(Bignum* res, Bignum const* lhs, Bignum const* rhs)
 {
   bn_assert(res);
   bn_assert(lhs);
   bn_assert(rhs);
 
-  struct bn sum;
+  Bignum sum;
   bignum_init(&sum);
 
   uint64_t carry =0;
 
   for(int i = 0; i < bn_array_size; ++i)
   {
-    struct bn row;
+    Bignum row;
     bignum_init(&row);
 
     carry = 0;
@@ -287,7 +426,7 @@ void bignum_mul(struct bn* res, struct bn* lhs, struct bn* rhs)
 }
 
 static uint64_t
-bignum__get_ndigits(struct bn *b)
+bignum__get_ndigits(Bignum const *b)
 {
   uint64_t ndigits = bn_array_size;
   do {
@@ -300,7 +439,7 @@ bignum__get_ndigits(struct bn *b)
 }
 
 static uint32_t
-bignum_divmod__getdigit(struct bn *r, struct bn *lhs, struct bn *rhs)
+bignum_divmod__getdigit(Bignum *r, Bignum const *lhs, Bignum const *rhs)
 {
   bn_assert(r);
   bn_assert(lhs);
@@ -316,11 +455,11 @@ bignum_divmod__getdigit(struct bn *r, struct bn *lhs, struct bn *rhs)
     uint32_t q0 = left / right;
     uint32_t r0 = left % right;
 
-    bignum_from_int(r, r0);
+    bignum_from_u64(r, r0);
     return q0;
   }
   else {
-    struct bn divident;
+    Bignum divident;
     bignum_assign(&divident, lhs);
 
     uint32_t quotient = 0;
@@ -335,7 +474,7 @@ bignum_divmod__getdigit(struct bn *r, struct bn *lhs, struct bn *rhs)
 }
 
 static void
-bignum__append_digit(struct bn *res, struct bn *num, uint32_t digit)
+bignum__append_digit(Bignum *res, Bignum const *num, uint32_t digit)
 {
   uint64_t i = bn_array_size;
   if(i != 1) do {
@@ -346,7 +485,7 @@ bignum__append_digit(struct bn *res, struct bn *num, uint32_t digit)
 }
 
 void
-bignum_divmod(struct bn* quot, struct bn *rem, struct bn* lhs, struct bn* rhs)
+bignum_divmod(Bignum* quot, Bignum *rem, Bignum const* lhs, Bignum const* rhs)
 {
   bn_assert(lhs);
   bn_assert(rhs);
@@ -354,7 +493,7 @@ bignum_divmod(struct bn* quot, struct bn *rem, struct bn* lhs, struct bn* rhs)
   bn_assert(rem);
 
   if(bignum_is_zero(rhs)) {
-    bn_assert(false);
+    bn_assert(0);
     return;
   }
 
@@ -367,12 +506,10 @@ bignum_divmod(struct bn* quot, struct bn *rem, struct bn* lhs, struct bn* rhs)
     return;
   }
 
-  struct bn divident;
-  struct bn quotient;
-  struct bn remainder;
+  Bignum divident;
+  Bignum quotient = {0};
+  Bignum remainder = {0};
 
-  bignum_from_int(&quotient, 0);
-  bignum_from_int(&remainder, 0);
   uint64_t ldigits = bignum__get_ndigits(lhs);
   uint64_t rdigits = bignum__get_ndigits(rhs);
   
@@ -394,8 +531,7 @@ bignum_divmod(struct bn* quot, struct bn *rem, struct bn* lhs, struct bn* rhs)
   }
 }
 
-
-int bignum_cmp(struct bn* a, struct bn* b)
+int bignum_cmp(Bignum const* a, Bignum const* b)
 {
   bn_assert(a);
   bn_assert(b);
@@ -418,33 +554,99 @@ int bignum_cmp(struct bn* a, struct bn* b)
   return 0;
 }
 
-
-int bignum_is_zero(struct bn* n)
+int bignum_greater(Bignum const* a, Bignum const* b)
 {
-  bn_assert(n);
+  bn_assert(a);
+  bn_assert(b);
 
-  int i;
-  for (i = 0; i < bn_array_size; ++i)
-  {
-    if (n->array[i])
-    {
+  int i = bn_array_size;
+  do {
+    -- i;
+    if(a->array[i] <= b->array[i]) {
       return 0;
     }
-  }
+  } while(i!=0);
 
   return 1;
 }
 
-void bignum_assign(struct bn* dst, struct bn* src)
+int bignum_less(Bignum const* a, Bignum const* b)
 {
-  bn_assert(dst);
-  bn_assert(src);
+  bn_assert(a);
+  bn_assert(b);
 
-  int i;
-  for (i = 0; i < bn_array_size; ++i)
-  {
-    dst->array[i] = src->array[i];
-  }
+  int i = bn_array_size;
+  do {
+    -- i;
+    if(a->array[i] >= b->array[i]) {
+      return 0;
+    }
+  } while(i!=0);
+
+  return 1;
+}
+
+int bignum_geq(Bignum const* a, Bignum const* b)
+{
+  bn_assert(a);
+  bn_assert(b);
+
+  int i = bn_array_size;
+  do {
+    -- i;
+    if(a->array[i] < b->array[i]) {
+      return 0;
+    }
+  } while(i!=0);
+
+  return 1;
+}
+
+int bignum_leq(Bignum const* a, Bignum const* b)
+{
+  bn_assert(a);
+  bn_assert(b);
+
+  int i = bn_array_size;
+  do {
+    -- i;
+    if(a->array[i] > b->array[i]) {
+      return 0;
+    }
+  } while(i!=0);
+
+  return 1;
+}
+
+int bignum_equal(Bignum const* a, Bignum const* b)
+{
+  bn_assert(a);
+  bn_assert(b);
+
+  int i = bn_array_size;
+  do {
+    -- i;
+    if(a->array[i] != b->array[i]) {
+      return 0;
+    }
+  } while(i!=0);
+
+  return 1;
+}
+
+int bignum_is_zero(Bignum const* n)
+{
+  bn_assert(n);
+
+  int i = bn_array_size;
+  do {
+    -- i;
+    if(n->array[i] != 0) {
+      return 0;
+    }
+  } while(i!=0);
+
+  return 1;
 }
 
 #endif
